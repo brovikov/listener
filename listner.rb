914cd39e17539153657fdb6f59af0d7da9960bcd
Alexey S. Brovikov <alexey.brovikov@gmail.com>
2017-01-24 17:26:01 +0530

    Update listner

--- listner.rb
+++ listner.rb
@@ -1,31 +1,50 @@
 require 'listen'
 require 'mail'
 require 'dotenv'
+require 'byebug'
 
-path =  File.dirname(__FILE__)
-Dotenv.load(path+'/.env')
+module Listner
 
-# this is myserver.rb
-# it does nothing really useful at the moment
+  path =  File.dirname(__FILE__)
+  Dotenv.load(path+'/.env')
 
-listener = Listen.to('/home/guest') do |modified, added, removed| 
-  puts added 
-  puts ENV['RECEIVER']
-  mail = Mail.new do
-    to ENV['RECEIVER']
-    from 'notify@partsable.com'
-    subject 'testing'
-    body "File #{added} had been added"
+  # this is myserver.rb
+  # it does nothing really useful at the moment
+
+  listener = Listen.to('/home/guest') do |modified, added, removed| 
+    puts added
+    if added.count > 0
+      added.each do |file|
+        puts ENV['RECEIVER']
+        mail = Mail.new do
+          to ENV['RECEIVER']
+          from 'notify@partsable.com'
+          subject "File #{file} has been added on #{Time.new.strftime("%m/%d/%Y at %I:%M%p")}"
+          body "Some description for #{file}"
+          add_file :filename => file.split('/').last, :content => File.read(file)
+        end
+        #Port 8025, 587 and 25 can also be used.
+        smtp = Net::SMTP.new('smtpcorp.com', 2525)
+        smtp.enable_starttls_auto
+        smtp.start( 
+          ENV['SMTPDOMAIN'], 
+          ENV['SMTPUSERNAME'], 
+          ENV['SMTPPASSWORD'], 
+          :plain
+        )
+        smtp.send_message(mail.to_s, 'notify@partsable.com', ENV['RECEIVER'])
+      end
+    end
   end
-  mail.deliver!
-end
 
-loop do
-  if listener.processing?
-    puts "Listener processing ..."
-  else
-    puts "Listener starting ..."
-    listener.start
-  end 
-  sleep(5)
+  loop do
+    if listener.processing?
+      puts "Listener processing ..."
+    else
+      puts "Listener starting ..."
+      listener.start
+    end 
+    sleep(5)
+  end
+
 end
